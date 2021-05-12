8bf94b70a333a7c45e144be2e03f122c7a043c1b
TomG308 <tgutierrez308@gmail.com>
2021-05-12 16:29:20 -0600

    connected price calc component to app component. Started form. Test add line function.

---
+++ src/components/PriceCalc.js
@@ -0,0 +1,32 @@
+import React, {component} from 'react'
+
+class PriceCalc extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state ={
+            numOne: Number,
+            NumTwo: Number,
+            answer: Number
+        }
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+
+    render() {
+        return(
+            <div>
+                <h2>H0i! priceCalc component</h2>
+            <form>
+                <label>
+                    testName:
+                    <input type="text" name="name" />
+                </label>
+                <input type="submit" value="submit" />    
+            </form>
+            </div>
+        )
+    }
+}
+export default PriceCalc
